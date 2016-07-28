729c2bc20d3d196a5851b5e5813a202824559a50
chenchen <chenchen@chenchens-macbook-air.local>
2016-07-29 01:20:30 +0800

    Initial Commit

---
+++ 异步下载网络图片 - 演练/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  异步下载网络图片 - 演练
+//
+//  Created by chenchen on 16/7/29.
+//  Copyright © 2016年 chenchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 异步下载网络图片 - 演练/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  异步下载网络图片 - 演练
+//
+//  Created by chenchen on 16/7/29.
+//  Copyright © 2016年 chenchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
