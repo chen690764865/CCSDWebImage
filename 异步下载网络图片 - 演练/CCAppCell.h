915580fbfa248e205043c82957e30f08b1363d9e
chenchen <690764865@qq.com>
2016-07-29 19:42:59 +0800

    基本布局

---
+++ 异步下载网络图片 - 演练/CCAppCell.h
@@ -0,0 +1,13 @@
+//
+//  CCAppCell.h
+//  异步下载网络图片 - 演练
+//
+//  Created by chenchen on 16/7/29.
+//  Copyright © 2016年 chenchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppCell : UITableViewCell
+
+@end
---
+++ 异步下载网络图片 - 演练/CCAppCell.m
@@ -0,0 +1,23 @@
+//
+//  CCAppCell.m
+//  异步下载网络图片 - 演练
+//
+//  Created by chenchen on 16/7/29.
+//  Copyright © 2016年 chenchen. All rights reserved.
+//
+
+#import "CCAppCell.h"
+
+@implementation CCAppCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
